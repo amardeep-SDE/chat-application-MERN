40c3c47e89450982656cfdbe2d1782877dbf221f
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-21 21:09:07 +0530

    feat: message controller and message routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
-import userRoute from './routes/userRoutes.js';
 import cookieParser from 'cookie-parser';
+import userRoute from './routes/userRoutes.js';
+import messageRoute from './routes/messageRoutes.js';
+
 dotenv.config({});
 
-const app = express();
+const app = express(); 
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +18,7 @@ app.use(cookieParser());
 //routes
 //http://localhost:8000/api/v1/user/register
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
 app.listen(PORT, () => {
     connectDB();
