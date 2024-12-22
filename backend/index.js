7443a698ab7a58213f2cef9b764381c50123c6e1
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-22 16:56:27 +0530

    feat: signup and login form, fix: app.js, index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import userRoute from './routes/userRoutes.js';
 import messageRoute from './routes/messageRoutes.js';
 
@@ -12,8 +13,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //middleweares
-app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
 app.use(cookieParser());
+const corsOption={
+  origin:'http://localhost:3000',
+  credentials:true
+};
+app.use(cors(corsOption)); 
 
 //routes
 //http://localhost:8000/api/v1/user/register
