d358c04ba21252629d1c497ef4fd680a39c6ad39
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2024-12-25 03:22:17 +0530

    fix: ready for production

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./socket/socket.js";
 dotenv.config({});
-
+import path from "path";
  
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+console.log(__dirname);
 
 // middleware
 app.use(express.urlencoded({extended:true}));
@@ -27,6 +30,15 @@ app.use(cors(corsOption));
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
  
+// app.use(express.static(path.join(DIRNAME,"/frontend/dist")));
+// app.use("*",(_,res) => {
+//     res.sendFile(path.resolve(DIRNAME, "frontend","dist","index.html"));
+// });
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 
 server.listen(PORT, ()=>{
     connectDB();
